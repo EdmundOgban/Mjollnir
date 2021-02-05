4e517b330c388a2af86dcb59dbb8a084d3f1f8b4
Edmund_Ogban <enrico@ambrox.net>
2021-02-05 10:24:10 +0100

    trio_driver: add perform, use bytearray instead of bytes, support bytes and str on _send

--- core/drivers/trio_driver.py
+++ core/drivers/trio_driver.py
@@ -30,6 +30,51 @@ class Driver:
         self._conn = None
         self._socket_connected = trio.Event()
 
+    async def connect(self):
+        if not self.connected:
+            await self._connect()
+
+    async def disconnect(self, reason=""):
+        if self.connected:
+            await self._send(commands.quit(reason))
+            await self._conn.aclose()
+            self._socket_connected = trio.Event()
+            self._conn = None
+
+    async def perform(self):
+        nick = self.identity["nick"]
+        await self._send(commands.nick(nick))
+
+    async def receive(self):
+        await self._socket_connected.wait()
+
+        s = bytearray()
+        while self._conn:
+            mesg = await self._conn.receive_some(4096)
+            if not mesg:
+                raise DriverConnectionError("Connection closed by peer")
+
+            if s:
+                mesg = s + mesg
+                s = bytearray()
+
+            for m in mesg.split(b'\n'):
+                if m.endswith(b'\r'):
+                    m = m.strip()
+                    if m:
+                        decoded = _decode(m)
+                        msg = parser.parse(decoded)
+                        yield msg
+                else:
+                    s += m
+
+    async def spool(self):
+        await self._socket_connected.wait()
+
+        while True:
+            msg = await self.rsendq.receive()
+            await self._send(msg)
+
     async def _connect(self):
         host = None
         port = None
@@ -57,64 +102,20 @@ class Driver:
         log.info(f"trio_driver Connected to {host}:{'+' if secure else ''}{port}")
         await self._identify()
 
-    async def connect(self):
-        if not self.connected:
-            await self._connect()
-
-    async def disconnect(self, reason=""):
-        await self._send(commands.quit(reason))
-        await self._conn.aclose()
-        self._socket_connected = trio.Event()
-        self._conn = None
-
-    async def receive(self):
-        await self._socket_connected.wait()
-
-        s = b''
-        while self._conn:
-            mesg = await self._conn.receive_some(4096)
-            if not mesg:
-                raise DriverConnectionError("Connection closed by peer")
-
-            if s:
-                mesg = s + mesg
-                s = b''
-
-            for m in mesg.split(b'\n'):
-                if m.endswith(b'\r'):
-                    m = m.strip()
-                    if m:
-                        decoded = _decode(m)
-                        msg = parser.parse(decoded)
-                        yield msg
-                else:
-                    s = m
-
-    async def spool(self):
-        await self._socket_connected.wait()
-
-        while True:
-            msg = await self.rsendq.receive()
-            await self._send(msg)
-
-    def _nicknames(self):
-        yield self.identity["nick"]
-        nick = self.identity["altnick"]
-        while True:
-            yield nick
-            nick = "{}{:02}".format("".join(random.sample(string.ascii_letters, 7)),
-                random.randint(0, 99))
-
-    # FIXME
     async def _identify(self):
         for msg in commands.identify(self.identity):
             await self._send(msg)
 
-        nick = self.identity["nick"]
-        await self._send(commands.nick(nick))
-
     async def _send(self, msg):
-        await self._conn.send_all(f"{msg}\r\n".encode("utf8"))
+        if isinstance(msg, str) or not msg.encoded:
+            s = f"{msg}\r\n".encode("utf8")
+        elif msg.encoded:
+            s = bytes(msg) + b"\r\n"
+        else:
+            log.warning(f"trio_driver._send Ignoring request to send '{msg}'")
+            return
+
+        await self._conn.send_all(s)
 
     @property
     def connected(self):
