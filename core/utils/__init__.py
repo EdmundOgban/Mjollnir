1c8b73a758daac40328b5ea314dbc7d74220eebb
Edmund_Ogban <enrico@ambrox.net>
2021-02-05 10:31:19 +0100

    Move core/utils.py to core/utils/__init__.py

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -36,6 +36,21 @@ def ischannel(arg, chantypes):
     return arg[0] in chantypes
 
 
+def morefmt(number):
+    #s = f" \x02({number} more message{{}})".format("s" if number != 1 else "")
+    s = f" \x02({number} more)"
+    return s
+
+def andify(L):
+    if not L:
+        return ""
+    elif len(L) == 1:
+        return L[0]
+    elif len(L) == 2:
+        return " and ".join(L)
+    else:
+        return ", ".join(L[:-1]) + f", and {L[-1]}"
+
+
 async def send(network, msg):
     await network._driver.wsendq.send(msg)
-
